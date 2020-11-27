c3bcc9dd1554f7ea72484696d69fc7be1469ac88
sebastian <sebastian_toro24131@elpoli.edu.co>
2020-11-26 23:24:30 -0500

    cambios despliegue heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/traduccion-galeradas'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/traduccion-galeradas/index.html'));});
+
+app.use(express.static(__dirname+'/dist/traduccion-galeradas'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/traduccion-galeradas/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
